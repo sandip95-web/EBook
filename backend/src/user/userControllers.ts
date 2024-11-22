6b1e1d0987933e28b76f8d0af8c7c25aa09f2dfa
sandip95-web <sandipmaharjan95@gmail.com>
2024-11-22 13:57:50 +0545

    User Model added

--- backend/src/user/userControllers.ts
+++ backend/src/user/userControllers.ts
@@ -1,17 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
-const registerUser=async(req:Request,res:Response,next:NextFunction)=>{
-  const {name,email,password}=req.body;
+const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, email, password } = req.body;
 
-  if(!name || !email || !password){
-    const error=createHttpError(400,"All fields are required");
-    return next(error)
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
   }
-  res.status(200).json({
-    success:true,
-    message:"User Created"
-  })
-}
 
-export {registerUser}
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User with that email already exist.");
+    return next(error);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "User Created",
+  });
+};
+
+export { registerUser };
---
+++ backend/src/user/userModel.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is Required."],
+  },
+  password: {
+    type: String,
+    required: [true, "Passsword is required."],
+  },
+});
+
+export default mongoose.model<User>("User", userSchema);
