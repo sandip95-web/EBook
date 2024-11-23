5947a2430a9ea9f46779bd394fde41cd67bad5de
sandip95-web <sandipmaharjan95@gmail.com>
2024-11-23 17:07:07 +0545

    add author id to new book

--- backend/src/book/bookController.ts
+++ backend/src/book/bookController.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
+import { AuthRequest } from "../config/authenticated";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const { title, genre } = req.body;
@@ -38,10 +39,11 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         filename_override: bookFileName,
       }
     );
+    const _req=req as AuthRequest
     const newBook = await bookModel.create({
       title,
       genre,
-      author: "674044051c19d031e55d482a",
+      author: _req.userId,
       coverImage: uploadResult.secure_url,
       file: bookFileUploadResult.secure_url,
     });
